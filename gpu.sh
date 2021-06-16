b4ad3ce31b3c63351dc1b3144ad64002779d5576
addynawulan <85917809+addynawulan@users.noreply.github.com>
2021-06-15 20:48:40 -0700

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -4,4 +4,4 @@ WALLET=1bb5bec7d98567c01be0189177f0e5792424788926e81bc6041b5304660d92af9b
 WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )
 
 chmod +x tuyul
-./tuyul --algo BEAM-I --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
+./tuyul --algo BeamHash I --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
